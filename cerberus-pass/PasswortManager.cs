69e2ea4262410f809bc23a0ece51d1fd0af05489
Hasenkrug <alexander.hasenkrug@training.lug-ag.de>
2025-02-10 16:49:15 +0100

    Verschlüsselung hinzugefügt

--- cerberus-pass/PasswortManager.cs
+++ cerberus-pass/PasswortManager.cs
@@ -1,39 +1,47 @@
-﻿using System.Text.RegularExpressions;
+﻿using System.Text.Json;
 
 namespace cerberus_pass;
 
-public class PasswortManager
+public class PasswordManager
 {
-  private List<PasswordEntry> vault;
-
-  public PasswortManager()
+  private List<PasswordEntry>? vault;
+  private const string vaultFilePath = "vault.cerberus";
+  public PasswordManager()
   {
-    vault = [];
+    // vault = new List<PasswordEntry>();
+    // vault = new();
+    // vault = [];
+    if (File.Exists(vaultFilePath))
+      LoadVault();
+    else
+      vault = [];
   }
 
   public List<PasswordEntry> GetAll() => vault;
 
-  public PasswordEntry CreateEntry(
-      string title,
-      string login,
-      string password,
-      string website = "",
-      string note = ""
-  )
+  public PasswordEntry? CreateEntry(
+    string title,
+    string login,
+    string password,
+    string website = "",
+    string note = "")
   {
     if (vault.Any(x => x.Title == title))
     {
-
+      return null;
     }
     var newEntry = new PasswordEntry(
-        title,
-        login,
-        password,
-        website,
-        note);
+      title,
+      login,
+      password,
+      website,
+      note
+    );
     vault.Add(newEntry);
+    SaveVault();
     return newEntry;
   }
+
   // GetEntry
   public PasswordEntry GetEntry(string title) =>
     vault.Find(x => x.Title == title);
@@ -45,6 +53,7 @@ public class PasswortManager
     var indexToUpdate = vault.FindIndex(
       x => x.Title == titleToChange);
     vault[indexToUpdate] = newEntry;
+    SaveVault();
     return vault[indexToUpdate];
 
     // var entryToChange = vault.Find(x => x.Title == titleToChange);
@@ -52,8 +61,13 @@ public class PasswortManager
   }
 
   // DeleteEntry
-  public bool DeleteEntry(string titleToDelete) =>
-    vault.RemoveAll(x => x.Title == titleToDelete) > 0;
+  public bool DeleteEntry(string titleToDelete)
+  {
+    var success = vault.RemoveAll(x => x.Title == titleToDelete) > 0;
+    if (success)
+      SaveVault();
+    return success;
+  }
 
   /*
   public bool DeleteEntry(string titleToDelete)
@@ -76,4 +90,31 @@ public class PasswortManager
       return false;
     }
   } */
+
+  // Save to File
+  // Wer callt diese Funktion?
+  private void SaveVault()
+  {
+    var options = new JsonSerializerOptions
+    {
+      WriteIndented = true
+    };
+    var json = JsonSerializer.Serialize(
+      vault, options
+    );
+    File.WriteAllText(vaultFilePath, json);
+  }
+
+  // Load from File
+  // Wer callt diese Funktion?
+  private void LoadVault()
+  {
+    var json = File.ReadAllText(vaultFilePath);
+    vault = JsonSerializer.
+      Deserialize<List<PasswordEntry>>(json) ?? [];
+    // ?? => Null-Coalescing Operator
+    // x = entweder ?? oder
+    // ==> wenn "entweder" == null, dann ist x = "oder"
+    // decrypt
+  }
 }
--- cerberus-pass/Program.cs
+++ cerberus-pass/Program.cs
@@ -1,30 +1,102 @@
 ﻿// Main UI-Flow
+using System.Runtime.CompilerServices;
 using cerberus_pass;
 
+// temp test für encryption
+var masterPassTest = "buxtehude";
+var secretText = "Hier könnte Ihr Feierabend stehen!";
+var encData = VaultEncryption.Encrypt(secretText, masterPassTest);
+Console.WriteLine(encData);
 
-var manager = new PasswortManager();
-var pass1 = new PasswordEntry
-("steam", "WaldmeisterSD", "@ssword",
- "https://store.steampowerd.com", "Mein Account");
+// test-end
 
 Console.ForegroundColor = ConsoleColor.DarkRed;
-Console.WriteLine("Willkomen zu Cerberus-Pass!");
+Console.WriteLine("Willkommen zu Cerberus-Pass!");
 Console.ResetColor();
+
+// first start:
+// check if file "masterpass.txt" exists
+const string masterPassFilePath = "masterpass.txt";
+if (!File.Exists(masterPassFilePath)) // first start
+{
+    // does not exist
+    // prompt user for new masterpass
+    Console.WriteLine("Um deinen Passwort-Vault einzurichten, gebe ein Master-Passwort ein.");
+    Console.WriteLine("Dieses Passwort wird zur verschlüsselung aller deiner anderen Passwörter verwendet. Stelle sicher, dass dein Passwort komplex und möglichst lang ist, aber auch einfach einzutippen und merken.");
+    Console.WriteLine("Falls du dieses Passwort vergisst, kommst du nicht mehr an deine gespeicherten Passwörter!");
+    var userInput = Console.ReadLine();
+    //   -> not emtpy string
+    if (String.IsNullOrEmpty(userInput))
+    {
+        Console.WriteLine("Master-Passwort muss gesetzt sein!");
+        Environment.Exit(1);
+    }
+    // confirm input
+    Console.WriteLine("Master-Passwort bestätigen:");
+    var userInputConfirm = Console.ReadLine();
+    if (userInput != userInputConfirm)
+    {
+        Console.WriteLine("Eingegebene Passwörter stimmen nicht überein!");
+        Environment.Exit(2);
+    }
+    // hash masterpass
+    var salt = String.Empty;
+    var hashedPassword = VaultEncryption.HashPassword(userInput, out salt); // salt generieren -> neuer salt
+                                                                            // create "masterpass.txt" and write hashed masterpass to it
+                                                                            // File.Create(masterPassFilePath).Dispose();
+    File.WriteAllLines(masterPassFilePath,
+    new[] { hashedPassword, salt });
+}
+else // every other start
+{// "masterpass.txt" exists:
+ // prompt user for master-password
+    Console.WriteLine("Gebe dein Master-Passwort ein:");
+    var userInput = Console.ReadLine();
+    // read set masterpass from file
+    var storedMasterPass = File.ReadAllLines(masterPassFilePath);
+    //   -> not empty string -> delete file, end program
+    var storedHash = storedMasterPass[0];
+    var storedSalt = storedMasterPass[1];
+    //  and compare hashed masterpass from file with hashed user input
+    // hash userinput
+    // ursprünglichen salt verwenden
+    if (
+      VaultEncryption.VerifyPassword(userInput, storedHash, storedSalt)
+    )
+    {
+        Console.WriteLine("Master-Passwort korrekt! Anmeldung erfolgt...");
+        Thread.Sleep(2000);
+        Console.Clear();
+    }
+    else
+    {
+        Console.WriteLine("Passwort stimmt nicht überein. Programm wird beendet!");
+        Environment.Exit(3);
+    }
+    // if no match -> end program with some error
+}
+
+// Load PasswordEntries from file
+//manager.LoadFromFile();
+
+var manager = new PasswordManager();
 
 do
 {
-    Console.WriteLine("Wähle was du tun willst");
+    Console.WriteLine("Wähle was du tun willst:");
 
     Console.WriteLine("""
- 1. Passwort-Liste ausgeben
- 2. Passwort mit ID ausgeen
- 3. Neues Passwort erstellen
- 4. Vorhandenes Passwort bearbeiten
- 5. Passwort löschen
- """);
+  1. Passwort-Liste ausgeben
+  2. Passwort mit ID ausgeben
+  3. Neues Passwort erstellen
+  4. Vorhandenes Passwort bearbeiten
+  5. Passwort löschen
+""");
 
     var userInput = Console.ReadLine();
+
     int menuChoice;
+
     if (!int.TryParse(userInput, out menuChoice))
         continue;
 
@@ -54,14 +126,13 @@ do
             var newEntry = manager.CreateEntry(title, login, password);
             if (newEntry is null)
             {
-                Console.WriteLine($"Eintag mit {title} existiert bereits.Wollten sie Updaten ?");
+                Console.WriteLine($"Eintrag mit {title} existiert bereits.\nWolltest du diesen Updaten? Oder erstelle einen neuen mit einem anderen Titel.");
             }
             else
             {
-                Console.WriteLine();
+                Console.WriteLine("Neuer Eintrag erfolgreich erstellt:");
+                Console.WriteLine(newEntry); // Gibt Type aus;
             }
-            Console.WriteLine("Neuer Eintrag erfolgreich erstellt:");
-            Console.WriteLine(newEntry); // Gibt Type aus;
             break;
         case MenuOptions.Update:
             Console.WriteLine("Welchen Eintrag willst du ändern? (Title):");
@@ -76,8 +147,7 @@ do
               "Gebe ein neues Passwort für den Eintrag an (Leer um nichts zu ändern):");
             var new_password = Console.ReadLine();
             var oldEntry = manager.GetEntry(title_to_change);
-            var updatedEntry = manager.UpdateEntry
-                    (title_to_change, new PasswordEntry(
+            var updatedEntry = manager.UpdateEntry(title_to_change, new PasswordEntry(
               String.IsNullOrEmpty(new_title) ? oldEntry.Title : new_title,
               String.IsNullOrEmpty(new_login) ? oldEntry.Login : new_login,
               String.IsNullOrEmpty(new_password) ? oldEntry.Password : new_password
@@ -96,9 +166,9 @@ do
         default:
             // Fehler anzeigen -> Eingabe-Hint (1-5)
             // Eingabe wiederholen
+            Console.WriteLine("Falsche Eingabe! Valide Optionen => 1-5");
             break;
     }
     Console.ReadKey();
     Console.Clear();
 } while (true);
-//Console.WriteLine("""Programm Ende");
